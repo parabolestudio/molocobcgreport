e89e08cfb870f9c1bdaaf3fd625a5c0831823b45
Kristin Baumann <hello@kristin-baumann.com>
2025-12-05 15:36:31 +0100

    Added journey path base

--- src/components/JourneySection.tsx
+++ src/components/JourneySection.tsx
@@ -149,24 +149,51 @@ export default function JourneySection() {
   );
 }
 
-function JourneyPath({ step }: { step: number }) {
+function JourneyPath() {
   return (
-    <div className="h-[300px] border border-red-500/50">
-      Journey Path {step}
+    <div className="max-h-[300]">
+      <svg
+        viewBox="0 0 1501 289"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        className="w-full h-full object-contain"
+      >
+        <path
+          d="M9.75 130.797V148.5V223.75C9.75 259.234 38.5157 288 74 288C109.484 288 138.25 259.234 138.25 223.75V73.5C138.25 37.8776 167.128 9 202.75 9C238.372 9 267.25 37.8776 267.25 73.5V222.695C267.25 258.647 296.601 287.684 332.552 287.296C368.072 286.913 396.62 257.924 396.459 222.402L396.25 176.137V148.677"
+          stroke="#60E2B7"
+          strokeWidth="1.5"
+        />
+        <path
+          d="M1037.75 106.013V223.75C1037.75 259.234 1066.52 288 1102 288C1137.48 288 1166.25 259.234 1166.25 223.75V73.5C1166.25 37.8776 1195.13 9 1230.75 9C1266.37 9 1295.25 37.8776 1295.25 73.5V222.695C1295.25 258.647 1324.6 287.684 1360.55 287.296C1396.07 286.913 1424.62 257.924 1424.46 222.402L1424.25 176.137V148.677"
+          stroke="#60E2B7"
+          strokeWidth="1.5"
+        />
+        <path
+          d="M1424.75 185.322V73.3496C1424.75 37.8103 1453.56 9 1489.1 9H1496.5"
+          stroke="#60E2B7"
+          strokeWidth="1.5"
+        />
+        <path
+          d="M395.75 191.695V73.1248C395.75 37.7097 424.46 9 459.875 9C495.29 9 524 37.7096 524 73.1248V223.625C524 259.178 552.821 288 588.375 288C623.928 288 652.75 259.178 652.75 223.625V130.089"
+          stroke="#60E2B7"
+          strokeWidth="1.5"
+        />
+        <path
+          d="M652.75 191.695V72.917C652.75 37.6167 681.367 9 716.667 9C751.967 9 780.584 37.6166 780.584 72.917V223.833C780.584 259.272 809.312 288 844.751 288C880.189 288 908.917 259.272 908.917 223.833V73.4164C908.917 37.8402 937.757 9 973.334 9C1008.91 9 1037.75 37.8402 1037.75 73.4164V191.695"
+          stroke="#60E2B7"
+          strokeWidth="1.5"
+        />
+        <circle cx="9.5" cy="126.5" r="9.5" fill="#60E2B7" />
+        <circle cx="1491.5" cy="9.5" r="9.5" fill="#60E2B7" />
+      </svg>
     </div>
   );
 }
-
 function JourneyContent({ step }: { step: number }) {
   return (
     <div className="grid grid-cols-[1fr_1.5fr_1fr] grid-rows-[1fr_1.5fr] gap-15">
-      <div
-        className=""
-        style={{
-          gridArea: "1 / 1 / 2 / 4",
-        }}
-      >
-        <JourneyPath step={step} />
+      <div style={{ gridArea: "1 / 1 / 2 / 4" }}>
+        <JourneyPath />
       </div>
       <div
         className="flex flex-col justify-end"
